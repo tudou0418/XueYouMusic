a0da4911dae257746ff274cfec40d19c23d866a6
tudou <642783485@qq.com>
2023-04-22 16:34:49 +0800

    task-轮播图组件使用

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="recommend" >
-    recommend
+    <Slider v-if="sliders.length" :sliders="sliders"></Slider>
   </div>
 </template>
 
 <script>
   import { getRecommend } from '@/service/recommend'
+  import Slider from '@/components/base/slider/slider'
   export default {
     name: 'recommend',
+    components: {
+      Slider
+    },
+    data() {
+      return {
+        sliders: []
+      }
+    }, 
     async created() {
       // 请求是异步的
      const result = await getRecommend()
+     this.sliders = result.sliders
      console.log('result', result)
     }
   }
